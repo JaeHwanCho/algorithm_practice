35d90265e5f411013c313f69f8b9202a3d8c8c79
JH <makefuture@kakao.com>
2021-08-11 22:26:49 +0900

    #1929

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@
 #include "#1051.h"
 #include "#1316.h"
 #include "#1652.h"
+#include "#1929.h"
 #include "#2438.h"
 #include "#11721.h"
 
 using namespace std;
 
 int main() {
-    num_1316();
+    num_1929();
 	return 0;
 }
