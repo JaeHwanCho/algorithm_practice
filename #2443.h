bfe71a92b6c45f64c4f35fba6d313e4ca5ed56a5
JH <makefuture@kakao.com>
2022-05-22 20:56:25 +0900

    #2443

---
+++ #2443.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+		for (int k = i + 1; k < n * 2 - i; k++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
